5e7091f4e018bdf6f8370f0e49df626d5f2e81c6
rajendra <rajendrammarboyana@gmail.com>
2024-03-15 11:58:50 +0000

    DEMO ON VARIABLES

--- 03-variable/var.tf
+++ 03-variable/var.tf
@@ -10,9 +10,21 @@ variable "y_list" {
   default = [9,0,8]
 }
 
+
+variable "d" {
+  default = {
+    m = 10
+    y = 20
+  }
+}
+
 #PRINT VARIABLe
 output "m" {
   value = var.m
+}
+
+output "d" {
+  value = "var.d_map"
 }
 
 output "y_list" {
