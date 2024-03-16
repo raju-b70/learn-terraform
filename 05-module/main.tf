f4723162ea861ca0aedb765e2ecdaa3605b8e916
rajendra <rajendrammarboyana@gmail.com>
2024-03-16 07:05:34 +0000

    module demo

--- 05-module/main.tf
+++ 05-module/main.tf
@@ -1,9 +1,11 @@
 module "demo-1" {
   source = "./demo"
   name = "demo-1"
+  instance_type = "t3.micro"
 }
 
 module "demo-2" {
   source = "./demo"
   name = "demo-2"
+  instance_type = "t3.small"
 }
