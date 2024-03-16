9b5361ff4ce47735b93ba078de8cc3a62c1326fc
rajendra <rajendrammarboyana@gmail.com>
2024-03-16 09:13:33 +0000

    expense v2

---
+++ expense-v2/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "frontend" {
+  for_each      = var.instance_types
+  ami           = var.ami
+  instance_type = each.value["instant_type"]
+  vpc_security_group_ids = var.security_group
+  tags = {
+    Name = "frontend"
+  }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_group" {
+  default = "sg-09998e9d2f260cf52"
+}
+
+variable "instance_types" {
+  default = {
+    frontend = {
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      instance_type = "t3.micro"
+    }
+    backend = {
+      instance_type = "t3.micro"
+    }
+
+
+
+
+
+  }
+}
