5d89b3260b7374991d935c6af11cd40da630e32c
rajendra <rajendrammarboyana@gmail.com>
2024-03-16 08:52:47 +0000

    loops

--- 07-loops/loop.tf
+++ 07-loops/loop.tf
@@ -1,3 +1,15 @@
 resource "null_resource" "test" {
   count = 10
 }
+resource "null_resource" "test1" {
+  for_each = var.colors-codes
+}
+variable "colors-codes" {
+  default = {
+    red = 21
+    green = 23
+    black= 26
+    orange = 29
+
+  }
+}
