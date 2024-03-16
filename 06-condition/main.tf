fbf7bfb7574515952b3b864a6302f60c43d1b78e
rajendra <rajendrammarboyana@gmail.com>
2024-03-16 08:09:06 +0000

    conditions

--- 06-condition/main.tf
+++ 06-condition/main.tf
@@ -1,9 +1,10 @@
 #condition is al about choosing the value
 #cond true-val ; fal-val
-resource "aws_instance" "test" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = var.instance_type == "t2*" ? "t3.micro" : var.instance_type
+#resource#aws_instance" "test" {
+ # ami           = "ami-05f020f5935e52dc4"
 
+variable "number" {}
 
+output "number" {
+  value = var.number > 10 ? "number is greater than 10" : "number is less than or equal to 10"
 }
-variable "instance_type" {}
