f465e6a82942da6922beeb4a5a745ca438fe55ab
rajendra <rajendrammarboyana@gmail.com>
2024-03-19 07:12:55 +0000

    provisioners

---
+++ 11-provisioners/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "test" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = DevOps321
+    host     = self.public_ip
+  }
+
+  inline = [
+    "sudo dnf install nginx -y",
+    "sudo systemct1 start nginx"
+  ]
+}
+}
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
