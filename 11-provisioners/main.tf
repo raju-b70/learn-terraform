dbc9c8e3e50a106bf24758cb8c65f070e08d3c3b
rajendra <rajendrammarboyana@gmail.com>
2024-03-19 08:28:14 +0000

    provisioners

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -25,3 +25,34 @@ data "aws_security_group" "selected" {
 }
 
 
+resource "aws_instance" "test1" {
+  ami                    = "ami-05f020f5935e52dc4"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+resource "null_resource""provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test1.public_ip
+    }
+
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
+
+
+
+
+
+
+
+
+
+
