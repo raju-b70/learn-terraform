5fef315c560a5e3495229cf05be72a6b6315da6b
rajendra <rajendrammarboyana@gmail.com>
2024-03-19 07:47:58 +0000

    provisioners

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -8,7 +8,7 @@ provisioner "remote-exec" {
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    password = DevOps321
+    password = "DevOps321"
     host     = self.public_ip
   }
 
